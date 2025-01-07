4c473c459e736ead3009fa31e62e897fdbd0efaf
Valentin Briukhanov <bvv.mag@gmail.com>
2025-01-07 16:37:41 +0100

    clear

--- default_html5/_generated_8cbc1b0c71f8f1cc.go
+++
@@ -1,5 +0,0 @@
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "default_animation: \"bg\"\nmaterial: \"/builtins/materials/sprite.material\"\ntextures {\n  sampler: \"texture_sampler\"\n  texture: \"/assets/background.atlas\"\n}\n"
-}
