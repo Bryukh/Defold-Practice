b60b20388960933fc52094ce8c6af08858c9c34f
Valentin Briukhanov <bvv.mag@gmail.com>
2025-01-07 16:30:09 +0100

    test build

---
+++ default_html5/_generated_8cbc1b0c71f8f1cc.go
@@ -0,0 +1,5 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"bg\"\nmaterial: \"/builtins/materials/sprite.material\"\ntextures {\n  sampler: \"texture_sampler\"\n  texture: \"/assets/background.atlas\"\n}\n"
+}
